87d158fd18b6939a02c184131cfa6d5176782a39
Mike Fikes <mike@fikesfarm.com>
2014-11-20 11:38:20 -0500

    Create new blank print-fibs projects in Clojure and ClojureScript

---
+++ Clojure/print-fibs/test/print_fibs/core_test.clj
@@ -0,0 +1,7 @@
+(ns print-fibs.core-test
+  (:require [clojure.test :refer :all]
+            [print-fibs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ ClojureScript/print-fibs/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to print-fibs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
