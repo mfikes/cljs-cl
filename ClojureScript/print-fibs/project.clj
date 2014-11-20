3e89886b0d210399958c437ca2562719ac38f85b
Mike Fikes <mike@fikesfarm.com>
2014-11-20 11:57:36 -0500

    Set up ClojureScript project

--- ClojureScript/print-fibs/project.clj
+++ ClojureScript/print-fibs/project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :cljsbuild {:builds {:rel {:source-paths ["src"]
+                             :compiler {:output-to     "js/main.js"
+                                        :optimizations :advanced}}}})
